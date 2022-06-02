2a1004ed0259a41b5f53c7bb626595546ee2677c
Chris Andaya <20297127+killuhwhale@users.noreply.github.com>
2022-06-01 22:22:37 -0700

    stuff done

--- osrs.py
+++ osrs.py
@@ -6,109 +6,24 @@ can locate it and click on it.
 
 Use:
     python3 osrs.py
-
 """
 import numpy as np
-import cv2
 import pyautogui
+from PIL import Image
+import cv2
 import random
 import time
-from PIL import Image
-import os
-import re
-import subprocess
 
 
-class Osrs:
-    def __init__(self):
-        self._pid = self._find_pid()
-        # Window ID of Runelite client.
-        self._win_id = self._get_win_ID()
-        
-        #Resizes window to set dimension.
-        self._resize_window()
-
+class OsrsClient:
+    def __init__(self, pid=None, dims=None):
+        # self._pid = self._find_pid()
+        self._pid = pid
         # A list to store dimensions: x, y postition, height and width of window.
         # All window capture cropping functionality is relative to these values.
-        self._dims = self._get_win_dimensions()
-        self._center_x = (self._dims[2] // 2) + self._dims[0] - 35
-        self._center_y = (self._dims[3] // 2) + self._dims[1] - 45
-
-    def _resize_window(self):
-        assert self._win_id, f"Window ID invalid: {self._win_id}"
-        cmd = f"xdotool windowsize {self._win_id} 600 600"
-        self._run_cmd(cmd)
-
-    def _get_win_dimensions(self):
-        """ Returns the postition of the top left corner of the window.
-        Returns the top left corner and the width and height of the window.
-
-        Returns:
-            values: A list of screen coordinates indicating window position and size.
-            [position x, position y, width, height]
-        """
-        _pos_pattern = r"""
-        Position:\s*(?P<p_x>\d*),(?P<p_y>\d*)\s*\(\w*:\s*\d*\)
-        """
-        _geo_pattern = r"""
-        Geometry:\s*(?P<g_x>\d*)x(?P<g_y>\d*)\s*
-        """
-        assert self._win_id, f"Window ID invalid: {self._win_id}"
-        pos_pattern = re.compile(_pos_pattern, re.MULTILINE | re.VERBOSE)
-        geo_pattern = re.compile(_geo_pattern, re.MULTILINE | re.VERBOSE)
-        cmd = f"xdotool getwindowgeometry {self._win_id}"
-        res = self._run_cmd(cmd)
-        pos_match = re.search(pos_pattern, res)
-        geo_match = re.search(geo_pattern, res)
-        px = pos_match.group('p_x')
-        py = pos_match.group('p_y')
-        gx = geo_match.group('g_x')
-        gy = geo_match.group('g_y')
-
-        # Position(top left corner x, y coord) and Geometry(widthxheight)
-        values = [px, py, gx, gy]
-        values = [int(val) for val in values]
-        assert all(values), f"Unable to find pos or geo of window: {self._win_id}"
-        return values
-
-
-    def _find_pid(self):
-        """ Finds PID of runelite.
-        #   Runelite 
-        #   10521 pts/0    Sl+   35:25 ./RuneLite.AppImage
-        """
-        top = self._run_cmd("ps ax")
-        pid_pattern = r"""^\s*
-        (?P<pid>\d*)\s*
-        \w*/\d*\s*
-        \w*\+\s*
-        \d*:\d*\s*
-        \.\/RuneLite\.AppImage$"""
-        pattern = re.compile(pid_pattern, re.MULTILINE | re.VERBOSE )
-        match = re.search(pattern, top)
-        assert match, f"Did not find pid: \n{top[:200]}"
-        return match.group('pid')
-
-    def _get_win_ID(self):
-        """ Gets window ID of Runelite.
-
-        Returns:
-            The window ID of Runelite based on the PID.
-            
-        """
-        assert self._pid, f"Window ID invalid: {self._pid}"
-        cmd = f"xdotool search --pid {self._pid}"
-        return self._run_cmd(cmd).split("\n")[-2]
-    
-    def _run_cmd(self, cmd):
-        """ Runs a bash command.
-
-        Args:
-            cmd: A string that represents the command.
-        Returns:
-            stdout from bash command
-        """
-        return subprocess.run(cmd.split(), check=True, encoding='utf-8', capture_output=True).stdout
+        self._dims = dims
+        self._center_x = (self._dims[2] // 2) + self._dims[0]
+        self._center_y = (self._dims[3] // 2) + self._dims[1]
 
     def _crop_inventory(self):
         """Crops the screenshot of the window to the Inventory.
@@ -129,12 +44,13 @@ class Osrs:
 
     def _screen_Image(self, left=0, top=0, right=0, bottom=0, name='screenshot.png'):
         myScreenshot = pyautogui.screenshot()
-        # open_cv_image = np.array(img) 
-        # # Convert RGB to BGR 
+        # open_cv_image = np.array(img)
+        # # Convert RGB to BGR
         # open_cv_image = open_cv_image[:, :, ::-1].copy()
         myScreenshot.save(r'screenshot.png')
         if left != 0 or top != 0 or right != 0 or bottom != 0:
-            im = Image.open(r'screenshot.png')  # uses PIL library to open image in memory
+            # uses PIL library to open image in memory
+            im = Image.open(r'screenshot.png')
             im = im.crop((left, top, right, bottom))  # defines crop points
             im.save(name)  # saves new cropped image
             print('screeenshot saved')
@@ -146,7 +62,7 @@ class Osrs:
             self._findarea_attack(3, 2)
             time.sleep(random.uniform(3, 8))
 
-    def  _closest(self, contour, x, y):
+    def _closest(self, contour, x, y):
         """  Finds the closest point on a contour to a given point.
         Args:
             countour: List of points indicating a contour.
@@ -167,7 +83,6 @@ class Osrs:
         """ Returns the closest point in a contour to the center of the screen.
         """
         return self._closest(contour, self._center_x, self._center_y)
-    
 
     def _closest_contour(self, contours):
         """ Finds the contour closest to the center of the screen.
@@ -197,28 +112,28 @@ class Osrs:
         x = dims[0]-10
         y = dims[1]-50
         image = image[y:y+h, x:x+w]
-        
-        # Convert RGB to BGR 
+
+        # Convert RGB to BGR
         image = image[:, :, ::-1].copy()
-        
+
         # B, G, R
-        #--------------------- ADD OBJECTS -------------------
+        # --------------------- ADD OBJECTS -------------------
         red = ([0, 0, 180], [80, 80, 255])
         green = ([0, 180, 0], [80, 255, 80])
-        pickup_high = ([200,0,100], [255,30,190])
+        pickup_high = ([200, 0, 100], [255, 30, 190])
         attack_blue = ([200, 200, 0], [255, 255, 50])
         amber = ([0, 160, 160], [80, 255, 255])
         # --------------------- ADD OBJECTS -------------------
         ore_list = [red, green, pickup_high, attack_blue, amber]
         boundaries = [ore_list[object]]
-        
+
         # loop over the boundaries
         for (lower, upper) in boundaries:
             # create NumPy arrays from the boundaries
             lower = np.array(lower, dtype="uint8")
             upper = np.array(upper, dtype="uint8")
             # Create black and white image where the highlighted color is/
-            
+
             # Find the color in the image
             mask = cv2.inRange(image, lower, upper)
             # Create blk and wht image using mask.
@@ -226,16 +141,17 @@ class Osrs:
             # Find threshold
             ret, thresh = cv2.threshold(mask, 50, 255, 0)
             # Get contours from threshold image.
-            contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-            
+            contours, hierarchy = cv2.findContours(
+                thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
         if len(contours) != 0:
             # cv2.drawContours(output, contours, -1, 255, 3)
             # find the biggest countour (c) by the area
-            
+
             # Find closest contour to the center of the screen.
             c_i = self._closest_contour(contours)
             c = contours[c_i]
-            
+
             # Drawg contour
             x, y, w, h = cv2.boundingRect(c)
             whalf = max(round(w/2), 1)
@@ -243,8 +159,10 @@ class Osrs:
             print(x, y, w, h)
 
             # cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            x = random.randrange(x + dims[0]-10 + whalf - deep, x + dims[0]-10 + max(whalf + deep, 1)) # 950,960
-            y = random.randrange(y + dims[1]-50 + hhalf - deep, y + dims[1]-50 + max(hhalf + deep, 1))  # 490,500
+            x = random.randrange(
+                x + dims[0]-10 + whalf - deep, x + dims[0]-10 + max(whalf + deep, 1))  # 950,960
+            y = random.randrange(
+                y + dims[1]-50 + hhalf - deep, y + dims[1]-50 + max(hhalf + deep, 1))  # 490,500
             print('attack x: ', x)
             print('attack y: ', y)
             b = random.uniform(0.05, 0.1)
@@ -258,7 +176,8 @@ class Osrs:
 
     def click_center(self):
         """Test center click. """
-        b = random.uniform(0.01, 0.05)
+        # print(f'Clicking: {self._center_x}, {self._center_y} ')
+        b = random.uniform(0.25, 0.5)
         pyautogui.moveTo(self._center_x, self._center_y, duration=b)
         pyautogui.click()
 
@@ -266,21 +185,25 @@ class Osrs:
         """Logs user into the game.
         User must be on the login screen ready to login.
         """
-        b = random.uniform(0.01, 0.05)
+        print(f"Loggin in w/ {username}, {password}")
+        b = random.uniform(0.05, 0.2)
         pyautogui.moveTo(self._center_x, self._center_y, duration=b)
-        pyautogui.click()
+        pyautogui.click()  # TODO() Make this click more generally instead of center
         msg = []
+        msg.append("enter")
         msg.extend(list(username))
         msg.append("enter")
+        pyautogui.typewrite(msg, interval=b)
+        msg = []
         msg.extend(list(password))
-        msg.append("enter")
-        pyautogui.typewrite(msg, interval=0.1)
+        # msg.append("enter")
+        pyautogui.typewrite(msg, interval=b)
+        msg = []
+
 
 if __name__ == "__main__":
     # NPC color indicator: #FF00FAFF
-    osrs = Osrs()
-    osrs.login("email@mail.com", "password")
-    osrs.attack()
-    
-
-    
+    # osrs = Osrs()
+    # osrs.login("email@mail.com", "password")
+    # osrs.attack()
+    print("")
