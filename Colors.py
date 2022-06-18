68ac4ab15745dd15d655dfa9da32b88a574e4edb
Chris Andaya <20297127+killuhwhale@users.noreply.github.com>
2022-06-17 21:59:09 -0700

    Colors and Items

---
+++ Colors.py
@@ -0,0 +1,51 @@
+from enum import Enum
+import numpy as np
+
+
+class Colors(Enum):
+    '''
+        Tile/ Object/ NPC Color indicators
+    '''
+
+    # Color = HSV Range  Low       High          Hex Color
+
+    NPC_PURPLE = {
+        'low': list([129, 187, 218]),
+        'hi': list([141, 208, 241]),
+        'hex_target': 'FFA434EE'
+    }
+    Yellow = {
+        'low': list([8, 231, 242]),
+        'hi': list([31, 255, 255]),
+        'hex_target': 'FFFFFF00'
+    }
+    Blue = {
+        'low': list([115, 240, 191]),
+        'hi': list([121, 255, 255]),
+        'hex_target': 'FF0000FF'
+    }
+    White = {
+        'low': list([0, 0, 192]),
+        'hi': list([1, 7, 255]),
+        'hex_target': 'FFFFFFFF'
+    }
+    Green = {
+        'low': list([61, 254, 0]),
+        'hi': list([69, 255, 255]),
+        'hex_target': 'FF00FF13'
+    }
+    Purple = {
+        'low': list([145, 254, 0]),
+        'hi': list([150, 255, 255]),
+        'hex_target': 'FFFF00FF'
+    }
+    Red = {
+        'low': list([0, 254, 0]),
+        'hi': list([1, 255, 255]),
+        'hex_target': 'FFFF0000'
+    }
+    Cyan = {
+        'low': list([71, 240, 85]),
+        'hi': list([94, 255, 255]),
+        'hex_target': 'FF00FFFF'
+    }
---
+++ Items.py
@@ -0,0 +1,87 @@
+from enum import Enum, unique
+
+
+@unique
+class Items(Enum):
+    '''
+        Map to Screenshot which are used as needle images to search in a space.
+    '''
+
+    AIR_RUNE = {"n": "AIR_RUNE", "conf": 0.80, "grayscale": False}
+    EARTH_RUNE = {"n": "EARTH_RUNE", "conf": 0.80, "grayscale": False}
+    WATER_RUNE = {"n": "WATER_RUNE", "conf": 0.80, "grayscale": False}
+    FIRE_RUNE = {"n": "FIRE_RUNE", "conf": 0.80, "grayscale": False}
+    LAW_RUNE = {"n": "LAW_RUNE", "conf": 0.80, "grayscale": False}
+    COSMIC_RUNE = {"n": "COSMIC_RUNE", "conf": 0.80, "grayscale": False}
+    BLOOD_RUNE = {"n": "BLOOD_RUNE", "conf": 0.80, "grayscale": False}
+    NATURE_RUNE = {"n": "NATURE_RUNE", "conf": 0.80, "grayscale": False}
+    DEATH_RUNE = {"n": "DEATH_RUNE", "conf": 0.80, "grayscale": False}
+    BODY_RUNE = {"n": "BODY_RUNE", "conf": 0.80, "grayscale": False}
+    CHAOS_RUNE = {"n": "CHAOS_RUNE", "conf": 0.80, "grayscale": False}
+    ASTRAL_RUNE = {"n": "ASTRAL_RUNE", "conf": 0.80, "grayscale": False}
+    SOUL_RUNE = {"n": "SOUL_RUNE", "conf": 0.80, "grayscale": False}
+    WRATH_RUNE = {"n": "WRATH_RUNE", "conf": 0.80, "grayscale": False}
+    DUST_RUNE = {"n": "DUST_RUNE", "conf": 0.80, "grayscale": False}
+    MUD_RUNE = {"n": "MUD_RUNE", "conf": 0.80, "grayscale": False}
+    SMOKE_RUNE = {"n": "SMOKE_RUNE", "conf": 0.80, "grayscale": False}
+    STEAM_RUNE = {"n": "STEAM_RUNE", "conf": 0.80, "grayscale": False}
+    LAVA_RUNE = {"n": "LAVA_RUNE", "conf": 0.80, "grayscale": False}
+    MIND_RUNE = {"n": "MIND_RUNE", "conf": 0.80, "grayscale": False}
+
+    RUNE_SKIM = {"n": "RUNE_SKIM", "conf": 0.80, "grayscale": False}
+    STAFF_OF_AIR = {"n": "STAFF_OF_AIR", "conf": 0.80, "grayscale": False}
+    STAFF_OF_FIRE = {"n": "STAFF_OF_FIRE", "conf": 0.80, "grayscale": False}
+    SWORDFISH = {"n": "SWORDFISH", "conf": 0.80, "grayscale": False}
+
+    BRONZE_BAR = {"n": "BRONZE_BAR", "conf": 0.80, "grayscale": False}
+    BLURITE_BAR = {"n": "BLURITE_BAR", "conf": 0.80, "grayscale": False}
+    IRON_BAR = {"n": "IRON_BAR", "conf": 0.80, "grayscale": False}
+    SILVER_BAR = {"n": "SILVER_BAR", "conf": 0.80, "grayscale": False}
+    ELEMENTAL_BAR = {"n": "ELEMENTAL_BAR", "conf": 0.80, "grayscale": False}
+    STEEL_BAR = {"n": "STEEL_BAR", "conf": 0.80, "grayscale": False}
+    GOLD_BAR = {"n": "GOLD_BAR", "conf": 0.80, "grayscale": False}
+    LOVAKITE_BAR = {"n": "LOVAKITE_BAR", "conf": 0.80, "grayscale": False}
+    MITHRIL_BAR = {"n": "MITHRIL_BAR", "conf": 0.80, "grayscale": False}
+    ADAMANTITE_BAR = {"n": "ADAMANTITE_BAR", "conf": 0.80, "grayscale": False}
+    RUNITE_BAR = {"n": "RUNITE_BAR", "conf": 0.80, "grayscale": False}
+
+    SAPPHIRE = {"n": "SAPPHIRE", "conf": 0.80, "grayscale": False}
+    EMERALD = {"n": "EMERALD", "conf": 0.80, "grayscale": False}
+    RUBY = {"n": "RUBY", "conf": 0.80, "grayscale": False}
+    DIAMOND = {"n": "DIAMOND", "conf": 0.80, "grayscale": False}
+    OPAL = {"n": "OPAL", "conf": 0.80, "grayscale": False}
+    RED_TOPAZ = {"n": "RED_TOPAZ", "conf": 0.80, "grayscale": False}
+    DRAGONSTONE = {"n": "DRAGONSTONE", "conf": 0.80, "grayscale": False}
+    ONYX = {"n": "ONYX", "conf": 0.80, "grayscale": False}
+    ZENYTE = {"n": "ZENYTE", "conf": 0.80, "grayscale": False}
+
+    SAPPHIRE_UNCUT = {"n": "SAPPHIRE_UNCUT", "conf": 0.80, "grayscale": False}
+    EMERALD_UNCUT = {"n": "EMERALD_UNCUT", "conf": 0.80, "grayscale": False}
+    RUBY_UNCUT = {"n": "RUBY_UNCUT", "conf": 0.80, "grayscale": False}
+    DIAMOND_UNCUT = {"n": "DIAMOND_UNCUT", "conf": 0.80, "grayscale": False}
+    OPAL_UNCUT = {"n": "OPAL_UNCUT", "conf": 0.80, "grayscale": False}
+    RED_TOPAZ_UNCUT = {"n": "RED_TOPAZ_UNCUT",
+                       "conf": 0.80, "grayscale": False}
+    DRAGONSTONE_UNCUT = {"n": "DRAGONSTONE_UNCUT",
+                         "conf": 0.80, "grayscale": False}
+    ONYX_UNCUT = {"n": "ONYX_UNCUT", "conf": 0.80, "grayscale": False}
+    ZENYTE_UNCUT = {"n": "ZENYTE_UNCUT", "conf": 0.80, "grayscale": False}
+
+    BRACELET_SAPPHIRE = {"n": "BRACELET_SAPPHIRE",
+                         "conf": 0.80, "grayscale": False}
+    BRACELET_EMERALD = {"n": "BRACELET_EMERALD",
+                        "conf": 0.80, "grayscale": False}
+    BRACELET_RUBY = {"n": "BRACELET_RUBY", "conf": 0.80, "grayscale": False}
+    BRACELET_DIAMOND = {"n": "BRACELET_DIAMOND",
+                        "conf": 0.80, "grayscale": False}
+    BRACELET_OPAL = {"n": "BRACELET_OPAL", "conf": 0.80, "grayscale": False}
+    BRACELET_RED_TOPAZ = {"n": "BRACELET_RED_TOPAZ",
+                          "conf": 0.80, "grayscale": False}
+    BRACELET_DRAGONSTONE = {"n": "BRACELET_DRAGONSTONE",
+                            "conf": 0.80, "grayscale": False}
+    BRACELET_ONYX = {"n": "BRACELET_ONYX", "conf": 0.80, "grayscale": False}
+    BRACELET_ZENYTE = {"n": "BRACELET_ZENYTE",
+                       "conf": 0.80, "grayscale": False}
+
+    def __str__(self):
+        return self.name
